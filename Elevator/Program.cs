a7a52104609c3d4c355ef9a3472153b2ccb0d0da
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-27 16:25:08 +0100

    Elevator

---
+++ Elevator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)capacity / people);
+            Console.WriteLine(courses);
+        }
+    }
+}
---
+++ ExcatSumOfRealNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcatSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                result += number;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
