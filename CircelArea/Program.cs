7704a1c60b2e8f9628af0705b3b9029b180c7812
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-27 16:16:34 +0100

    first commit

---
+++ CircelArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircelArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("{0:f12}", area);
+        }
+    }
+}
---
+++ DayOfWeek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            Console.WriteLine(days[n-1]);
+        }
+    }
+}
