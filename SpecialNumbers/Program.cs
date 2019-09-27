d24d9a046d325083dce3e983ff33dcbb12e7d5e2
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-27 17:04:19 +0100

    variabletypes

--- SpecialNumbers/Program.cs
+++ SpecialNumbers/Program.cs
@@ -10,6 +10,23 @@ namespace SpecialNumbers
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int number = i;
+                bool isEqual = false;
+                while(number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                if(sum == 5 || sum == 7 || sum == 11)
+                {
+                    isEqual = true;
+                }
+                Console.WriteLine("{0} -> {1}", i, isEqual);
+            }
         }
     }
 }
---
+++ TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char letter = (char)('a' + n);
+            for (char l1 = 'a'; l1 < letter; l1++)
+            {
+                for (char l2 = 'a'; l2 < letter; l2++)
+                {
+                    for (char l3 = 'a'; l3 < letter; l3++)
+                    {
+                        Console.WriteLine("{0}{1}{2}", l1, l2, l3);
+                    }
+                }
+            }
+        }
+    }
+}
