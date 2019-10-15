771ea7e7beafc63648de8a8a1e269ef2211bc51e
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-10-15 18:53:50 +0100

    more exercises

---
+++ RemoveNegativesAndREverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNegativesAndREverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> result =  new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if(numbers[i] >= 0)
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+            result.Reverse();
+            if(result.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
